1f53d083852192cea7a720505dd11a56458db35b
andreLake <andre.lake@hotmail.com>
2024-02-13 19:34:17 -0300

    set basic main

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+def main() -> None:
+    print(OPENAI_API_KEY)
+
+
+if __name__ == "__main__":
+    main()
